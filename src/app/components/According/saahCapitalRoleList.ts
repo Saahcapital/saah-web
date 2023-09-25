7817198db4690ab9cc50f0fd12779982a46aaa53
ubihacks <ubaidifti@gmail.com>
2023-09-25 17:42:08 +0500

    roles change

--- src/app/components/According/saahCapitalRoleList.ts
+++ src/app/components/According/saahCapitalRoleList.ts
@@ -13,27 +13,28 @@ export const saahCapitalRoleListings = [
     },
     {
       uid: 'role003',
+      title: 'VISION 2030 Attracted more foreign investment',
+      description:
+        'Saah Capital involves implementing reforms, modernizing infrastructure, and increasing transparency to create a more attractive and competitive marketplace for investors.',
+    },
+    {
+      uid: 'role004',
       title: 'Flourishing market indices',
       description:
         'Saah Capital recognizes the potential for further growth in the Saudi market and provides local investors and traders with sophisticated financial tools to capitalize on these opportunities.',
     },
      {
-      uid: 'role004',
+      uid: 'role005',
       title: '44% annual increase in MENA IPOs',
       description:
         'Saah Capital actively participates in IPO investments, leveraging AI algorithms to identify lucrative opportunities and offering financial tools that enhance investor success.',
     }, {
-      uid: 'role005',
+      uid: 'role006',
       title: 'Oversubscribed IPOs in Saudi Arabia',
       description:
         'Saah Capital acts as a bridge between investors and oversubscribed IPOs, ensuring that clients can access these high-demand offerings efficiently.',
     },
-    {
-      uid: 'role006',
-      title: 'VISION 2030 Attracted more foreign investment',
-      description:
-        'involves implementing reforms, modernizing infrastructure, and increasing transparency to create a more attractive and competitive marketplace for investors.',
-    },
+   
   
   ]
   
